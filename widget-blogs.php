c92c6d15c19b7492d46c31ff689048bdc973a73f
moha <moha@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-04-06 21:59:48 +0000

    - Fixed: Undefined function 'get_blog_avatar'
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/blogs-widget@3059 f53fe46e-8c02-459b-9fdd-93e75771b278

--- widget-blogs.php
+++ widget-blogs.php
@@ -4,7 +4,7 @@ Plugin Name: Blogs Widget
 Plugin URI: http://premium.wpmudev.org/project/footer-content
 Description: Show recently updated blogs across your site, with avatars, through this handy widget
 Author: S H Mohanjith (Incsub), Andrew Billits (Incsub)
-Version: 1.0.2
+Version: 1.0.3
 Author URI: http://premium.wpmudev.org
 WDP ID: 64
 Network: true
@@ -32,7 +32,6 @@ add_action('init', 'widget_blogs_init');
 function widget_blogs_init() {
 	if ( !is_multisite() )
 		exit( 'The Widget Blogs plugin is only compatible with WordPress Multisite.' );
-		
 	load_plugin_textdomain('widget_blogs', false, dirname(plugin_basename(__FILE__)).'/languages');
 }
 
@@ -49,7 +48,7 @@ $blogs_widget_main_blog_only = 'no'; //Either 'yes' or 'no'
 //------------------------------------------------------------------------//
 function widget_blogs_widget_init() {
 	global $wpdb, $blogs_widget_main_blog_only;
-		
+	
 	// Check for the required API functions
 	if ( !function_exists('register_sidebar_widget') || !function_exists('register_widget_control') )
 		return;
@@ -72,17 +71,21 @@ function widget_blogs_widget_init() {
 		}
 	?>
 				<div style="text-align:left">
-                
+        
 				<label for="blogs-title" style="line-height:35px;display:block;"><?php _e('Title', 'widgets', 'widget_blogs'); ?>:<br />
                 <input class="widefat" id="blogs-title" name="blogs-title" value="<?php echo $options['blogs-title']; ?>" type="text" style="width:95%;">
                 </select>
                 </label>
 				<label for="blogs-display" style="line-height:35px;display:block;"><?php _e('Display', 'widgets', 'widget_blogs'); ?>:
+<?php if (function_exists('get_blog_avatar')) { ?>
                 <select name="blogs-display" id="blogs-display" style="width:95%;">
                 <option value="avatar_blog_name" <?php if ($options['blogs-display'] == 'avatar_blog_name'){ echo 'selected="selected"'; } ?> ><?php _e('Avatar + Blog Name', 'widget_blogs'); ?></option>
                 <option value="avatar" <?php if ($options['blogs-display'] == 'avatar'){ echo 'selected="selected"'; } ?> ><?php _e('Avatar Only', 'widget_blogs'); ?></option>
                 <option value="blog_name" <?php if ($options['blogs-display'] == 'blog_name'){ echo 'selected="selected"'; } ?> ><?php _e('Blog Name Only', 'widget_blogs'); ?></option>
                 </select>
+<?php } else { ?>
+		<input type="hidden" name="blogs-display" id="blogs-display" value="blog_name" />
+<?php } ?>
                 </label>
 				<label for="blogs-blog-name-characters" style="line-height:35px;display:block;"><?php _e('Blog Name Characters', 'widgets', 'widget_blogs'); ?>:<br />
                 <select name="blogs-blog-name-characters" id="blogs-blog-name-characters" style="width:95%;">
@@ -167,15 +170,15 @@ function widget_blogs_widget_init() {
 					}
 					foreach ($blogs as $blog){
 						$blog_details = get_blog_details( $blog['blog_id'] );
-						if ( $options['blogs-display'] == 'avatar_blog_name' ) {
+						if ( $options['blogs-display'] == 'avatar_blog_name' && function_exists('get_blog_avatar')) {
 							echo '<li>';
 							echo '<a href="' . $blog_details->siteurl . '">' . get_blog_avatar( $blog['blog_id'], $options['blogs-avatar-size'], '' ) . '</a>';
 							echo ' ';
 							echo '<a href="' . $blog_details->siteurl . '">' . substr($blog_details->blogname, 0, $options['blogs-blog-name-characters']) . '</a>';
 							echo '</li>';
-						} else if ( $options['blogs-display'] == 'avatar' ) {
+						} else if ( $options['blogs-display'] == 'avatar' && function_exists('get_blog_avatar')) {
 							echo '<a href="' . $blog_details->siteurl . '">' . get_blog_avatar( $blog['blog_id'], $options['blogs-avatar-size'], '' ) . '</a>';
-						} else if ( $options['blogs-display'] == 'blog_name' ) {
+						} else if ( $options['blogs-display'] == 'blog_name' || !function_exists('get_blog_avatar') ) {
 							echo '<li>';
 							echo '<a href="' . $blog_details->siteurl . '">' . substr($blog_details->blogname, 0, $options['blogs-blog-name-characters']) . '</a>';
 							echo '</li>';
