6cd9c2c37ecc652c92e4a4ec6a7b02303bdb639f
moha <moha@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-05-07 01:31:58 +0000

    - Multiple blogs widgets
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/blogs-widget@3368 f53fe46e-8c02-459b-9fdd-93e75771b278

--- widget-blogs.php
+++ widget-blogs.php
@@ -50,98 +50,36 @@ function widget_blogs_widget_init() {
 	global $wpdb, $blogs_widget_main_blog_only;
 	
 	// Check for the required API functions
-	if ( !function_exists('register_sidebar_widget') || !function_exists('register_widget_control') )
+	if ( !function_exists('register_widget') )
 		return;
+	
+	register_widget('BlogsWidget');
+}
 
-	// This saves options and prints the widget's config form.
-	function widget_blogs_control() {
-		global $wpdb;
-		$options = $newoptions = get_option('widget_blogs');
-		if ( $_POST['blogs-submit'] ) {
-			$newoptions['blogs-title'] = $_POST['blogs-title'];
-			$newoptions['blogs-display'] = $_POST['blogs-display'];
-			$newoptions['blogs-blog-name-characters'] = $_POST['blogs-blog-name-characters'];
-			$newoptions['blogs-order'] = $_POST['blogs-order'];
-			$newoptions['blogs-number'] = $_POST['blogs-number'];
-			$newoptions['blogs-avatar-size'] = $_POST['blogs-avatar-size'];
-		}
-		if ( $options != $newoptions ) {
-			$options = $newoptions;
-			update_option('widget_blogs', $options);
-		}
-	?>
-				<div style="text-align:left">
-        
-				<label for="blogs-title" style="line-height:35px;display:block;"><?php _e('Title', 'widgets', 'widget_blogs'); ?>:<br />
-                <input class="widefat" id="blogs-title" name="blogs-title" value="<?php echo $options['blogs-title']; ?>" type="text" style="width:95%;">
-                </select>
-                </label>
-				<label for="blogs-display" style="line-height:35px;display:block;"><?php _e('Display', 'widgets', 'widget_blogs'); ?>:
-<?php if (function_exists('get_blog_avatar')) { ?>
-                <select name="blogs-display" id="blogs-display" style="width:95%;">
-                <option value="avatar_blog_name" <?php if ($options['blogs-display'] == 'avatar_blog_name'){ echo 'selected="selected"'; } ?> ><?php _e('Avatar + Blog Name', 'widget_blogs'); ?></option>
-                <option value="avatar" <?php if ($options['blogs-display'] == 'avatar'){ echo 'selected="selected"'; } ?> ><?php _e('Avatar Only', 'widget_blogs'); ?></option>
-                <option value="blog_name" <?php if ($options['blogs-display'] == 'blog_name'){ echo 'selected="selected"'; } ?> ><?php _e('Blog Name Only', 'widget_blogs'); ?></option>
-                </select>
-<?php } else { ?>
-		<input type="hidden" name="blogs-display" id="blogs-display" value="blog_name" />
-<?php } ?>
-                </label>
-				<label for="blogs-blog-name-characters" style="line-height:35px;display:block;"><?php _e('Blog Name Characters', 'widgets', 'widget_blogs'); ?>:<br />
-                <select name="blogs-blog-name-characters" id="blogs-blog-name-characters" style="width:95%;">
-                <?php
-					if ( empty($options['blogs-blog-name-characters']) ) {
-						$options['blogs-blog-name-characters'] = 30;
-					}
-					$counter = 0;
-					for ( $counter = 1; $counter <= 500; $counter += 1) {
-						?>
-                        <option value="<?php echo $counter; ?>" <?php if ($options['blogs-blog-name-characters'] == $counter){ echo 'selected="selected"'; } ?> ><?php echo $counter; ?></option>
-                        <?php
-					}
-                ?>
-                </select>
-                </label>
-				<label for="blogs-order" style="line-height:35px;display:block;"><?php _e('Order', 'widgets', 'widget_blogs'); ?>:
-                <select name="blogs-order" id="blogs-order" style="width:95%;">
-                <option value="most_recent" <?php if ($options['blogs-order'] == 'most_recent'){ echo 'selected="selected"'; } ?> ><?php _e('Most Recent', 'widget_blogs'); ?></option>
-                <option value="random" <?php if ($options['blogs-order'] == 'random'){ echo 'selected="selected"'; } ?> ><?php _e('Random', 'widget_blogs'); ?></option>
-                </select>
-                </label>
-				<label for="blogs-number" style="line-height:35px;display:block;"><?php _e('Number', 'widgets', 'widget_blogs'); ?>:<br />
-                <select name="blogs-number" id="blogs-number" style="width:95%;">
-                <?php
-					if ( empty($options['blogs-number']) ) {
-						$options['blogs-number'] = 10;
-					}
-					$counter = 0;
-					for ( $counter = 1; $counter <= 25; $counter += 1) {
-						?>
-                        <option value="<?php echo $counter; ?>" <?php if ($options['blogs-number'] == $counter){ echo 'selected="selected"'; } ?> ><?php echo $counter; ?></option>
-                        <?php
-					}
-                ?>
-                </select>
-                </label>
-				<label for="blogs-avatar-size" style="line-height:35px;display:block;"><?php _e('Avatar Size', 'widgets', 'widget_blogs'); ?>:<br />
-                <select name="blogs-avatar-size" id="blogs-avatar-size" style="width:95%;">
-                <option value="16" <?php if ($options['blogs-avatar-size'] == '16'){ echo 'selected="selected"'; } ?> ><?php _e('16px', 'widget_blogs'); ?></option>
-                <option value="32" <?php if ($options['blogs-avatar-size'] == '32'){ echo 'selected="selected"'; } ?> ><?php _e('32px', 'widget_blogs'); ?></option>
-                <option value="48" <?php if ($options['blogs-avatar-size'] == '48'){ echo 'selected="selected"'; } ?> ><?php _e('48px', 'widget_blogs'); ?></option>
-                <option value="96" <?php if ($options['blogs-avatar-size'] == '96'){ echo 'selected="selected"'; } ?> ><?php _e('96px', 'widget_blogs'); ?></option>
-                <option value="128" <?php if ($options['blogs-avatar-size'] == '128'){ echo 'selected="selected"'; } ?> ><?php _e('128px', 'widget_blogs'); ?></option>
-                </select>
-                </label>
-				<input type="hidden" name="blogs-submit" id="blogs-submit" value="1" />
-				</div>
-	<?php
-	}
-// This prints the widget
-	function widget_blogs($args) {
-		global $wpdb, $current_site;
-		extract($args);
-		$defaults = array('count' => 10, 'blogname' => 'wordpress');
-		$options = (array) get_option('widget_blogs');
+add_action('widgets_init', 'widget_blogs_widget_init');
+
+class BlogsWidget extends WP_Widget {
+    
+    var $translation_domain = 'widget_blogs';
+    
+    function BlogsWidget() {
+	$widget_ops = array( 'description' => __('Display Blogs Pages', $this->translation_domain) );
+        $control_ops = array( 'title' => __('Blogs', $this->translation_domain),
+		       'display' => 'blog_name', 'blog-name-characters' => 30,
+		       'order' => 'random', 'number' => 10,
+		       'avatar-size' => 16
+		       );
+	
+        $this->WP_Widget( 'blogs_widget', __('Blogs', $this->translation_domain), $widget_ops, $control_ops );
+    }
+    
+    function widget($args, $instance) {
+	global $wpdb, $current_site, $post, $blogs_tree;
+	
+	extract($args);
+	
+	$defaults = array('count' => 10, 'blogname' => 'wordpress');
+	$options = $instance;
 
 		foreach ( $defaults as $key => $value )
 			if ( !isset($options[$key]) )
@@ -149,63 +87,146 @@ function widget_blogs_widget_init() {
 
 		?>
 		<?php echo $before_widget; ?>
-			<?php echo $before_title . __($options['blogs-title'], 'widget_blogs') . $after_title; ?>
+			<?php echo $before_title . __($options['title'], 'widget_blogs') . $after_title; ?>
             <br />
             <?php
-
-			$newoptions['blogs-display'] = $_POST['blogs-display'];
-			$newoptions['blogs-order'] = $_POST['blogs-order'];
-			$newoptions['blogs-number'] = $_POST['blogs-number'];
-			$newoptions['blogs-avatar-size'] = $_POST['blogs-avatar-size'];
 				//=================================================//
-				if ( $options['blogs-order'] == 'most_recent' ) {
-					$query = "SELECT blog_id FROM " . $wpdb->base_prefix . "blogs WHERE site_id = '" . $wpdb->siteid . "' AND spam != '1' AND archived != '1' AND deleted != '1' ORDER BY registered DESC LIMIT " . $options['blogs-number'];
-				} else if ( $options['blogs-order'] == 'random' ) {
-					$query = "SELECT blog_id FROM " . $wpdb->base_prefix . "blogs WHERE site_id = '" . $wpdb->siteid . "' AND spam != '1' AND archived != '1' AND deleted != '1' ORDER BY RAND() LIMIT " . $options['blogs-number'];
+				if ( $options['-order'] == 'most_recent' ) {
+					$query = "SELECT blog_id FROM " . $wpdb->base_prefix . "blogs WHERE site_id = '" . $wpdb->siteid . "' AND spam != '1' AND archived != '1' AND deleted != '1' ORDER BY registered DESC LIMIT " . $options['number'];
+				} else if ( $options['order'] == 'random' ) {
+					$query = "SELECT blog_id FROM " . $wpdb->base_prefix . "blogs WHERE site_id = '" . $wpdb->siteid . "' AND spam != '1' AND archived != '1' AND deleted != '1' ORDER BY RAND() LIMIT " . $options['number'];
 				}
 				$blogs = $wpdb->get_results( $query, ARRAY_A );
 				if (count($blogs) > 0){
-					if ( $options['blogs-display'] == 'blog_name' || $options['blogs-display'] == 'avatar_blog_name' ) {
+					if ( $options['display'] == 'blog_name' || $options['display'] == 'avatar_blog_name' ) {
 						echo '<ul>';
 					}
 					foreach ($blogs as $blog){
 						$blog_details = get_blog_details( $blog['blog_id'] );
-						if ( $options['blogs-display'] == 'avatar_blog_name' && function_exists('get_blog_avatar')) {
+						if ( $options['display'] == 'avatar_blog_name' && function_exists('get_blog_avatar')) {
 							echo '<li>';
-							echo '<a href="' . $blog_details->siteurl . '">' . get_blog_avatar( $blog['blog_id'], $options['blogs-avatar-size'], '' ) . '</a>';
+							echo '<a href="' . $blog_details->siteurl . '">' . get_blog_avatar( $blog['blog_id'], $options['avatar-size'], '' ) . '</a>';
 							echo ' ';
-							echo '<a href="' . $blog_details->siteurl . '">' . substr($blog_details->blogname, 0, $options['blogs-blog-name-characters']) . '</a>';
+							echo '<a href="' . $blog_details->siteurl . '">' . substr($blog_details->blogname, 0, $options['blog-name-characters']) . '</a>';
 							echo '</li>';
-						} else if ( $options['blogs-display'] == 'avatar' && function_exists('get_blog_avatar')) {
-							echo '<a href="' . $blog_details->siteurl . '">' . get_blog_avatar( $blog['blog_id'], $options['blogs-avatar-size'], '' ) . '</a>';
-						} else if ( $options['blogs-display'] == 'blog_name' || !function_exists('get_blog_avatar') ) {
+						} else if ( $options['display'] == 'avatar' && function_exists('get_blog_avatar')) {
+							echo '<a href="' . $blog_details->siteurl . '">' . get_blog_avatar( $blog['blog_id'], $options['avatar-size'], '' ) . '</a>';
+						} else if ( $options['display'] == 'blog_name' || !function_exists('get_blog_avatar') ) {
 							echo '<li>';
-							echo '<a href="' . $blog_details->siteurl . '">' . substr($blog_details->blogname, 0, $options['blogs-blog-name-characters']) . '</a>';
+							echo '<a href="' . $blog_details->siteurl . '">' . substr($blog_details->blogname, 0, $options['blog-name-characters']) . '</a>';
 							echo '</li>';
 						}
 					}
-					if ( $options['blogs-display'] == 'blog_name' || $options['blogs-display'] == 'avatar_blog_name' ) {
+					if ( $options['display'] == 'blog_name' || $options['display'] == 'avatar_blog_name' ) {
 						echo '</ul>';
 					}
 				}
 				//=================================================//
 			?>
 		<?php echo $after_widget; ?>
-<?php
-	}
-	// Tell Dynamic Sidebar about our new widget and its control
-	if ( $blogs_widget_main_blog_only == 'yes' ) {
-		if ( $wpdb->blogid == 1 ) {
-			register_sidebar_widget(array(__('Blogs', 'widget_blogs'), 'widgets'), 'widget_blogs');
-			register_widget_control(array(__('Blogs', 'widget_blogs'), 'widgets'), 'widget_blogs_control');
-		}
-	} else {
-		register_sidebar_widget(array(__('Blogs', 'widget_blogs'), 'widgets'), 'widget_blogs');
-		register_widget_control(array(__('Blogs', 'widget_blogs'), 'widgets'), 'widget_blogs_control');
-	}
+	<?php
+    }
+    
+    function update($new_instance, $old_instance) {
+	$instance = $old_instance;
+        $new_instance = wp_parse_args( (array) $new_instance, array( 'title' => __('Blogs', $this->translation_domain),
+		       'display' => 'blog_name', 'blog-name-characters' => 30,
+		       'order' => 'random', 'number' => 10,
+		       'avatar-size' => 16
+		       ) );
+	$instance['title'] = $new_instance['title'];
+	$instance['display'] = $new_instance['display'];
+	$instance['blog-name-characters'] = $new_instance['blog-name-characters'];
+	$instance['order'] = $new_instance['order'];
+	$instance['number'] = $new_instance['number'];
+	$instance['avatar-size'] = $new_instance['avatar-size'];
+	
+        return $instance;
+    }
+    
+    function form($instance) {
+	$instance = wp_parse_args( (array) $instance,
+		array( 'title' => __('Blogs', $this->translation_domain),
+		       'display' => 'blog_name', 'blog-name-characters' => 30,
+		       'order' => 'random', 'number' => 10,
+		       'avatar-size' => 16
+		       ));
+        $options = array('title' => strip_tags($instance['title']), 'display' => $instance['display'],
+			 'blog-name-characters' => $instance['blog-name-characters'],
+			 'order' => $instance['order'],
+			 'number' => $instance['number'],
+			 'avatar-size' => $instance['avatar-size']);
+	
+	?>
+	<div style="text-align:left">
+		<label for="blogs-title" style="line-height:35px;display:block;"><?php _e('Title', 'widgets', 'widget_blogs'); ?>:<br />
+			<input class="widefat" id="blogs-title" name="title" value="<?php echo $options['title']; ?>" type="text" style="width:95%;" />
+                </label>
+		<?php if (function_exists('get_blog_avatar')) { ?>
+			<label for="blogs-isplay" style="line-height:35px;display:block;"><?php _e('Display', 'widgets', 'widget_blogs'); ?>:
+			<select name="display" id="blogs-display" style="width:95%;">
+				<option value="avatar_blog_name" <?php if ($options['display'] == 'avatar_blog_name'){ echo 'selected="selected"'; } ?> ><?php _e('Avatar + Blog Name', 'widget_blogs'); ?></option>
+				<option value="avatar" <?php if ($options['display'] == 'avatar'){ echo 'selected="selected"'; } ?> ><?php _e('Avatar Only', 'widget_blogs'); ?></option>
+				<option value="blog_name" <?php if ($options['display'] == 'blog_name'){ echo 'selected="selected"'; } ?> ><?php _e('Blog Name Only', 'widget_blogs'); ?></option>
+			</select>
+		<?php } else { ?>
+			<input type="hidden" name="display" id="blogs-display" value="blog_name" />
+		<?php } ?>
+                </label>
+		<label for="blogs-blog-name-characters" style="line-height:35px;display:block;"><?php _e('Blog Name Characters', 'widgets', 'widget_blogs'); ?>:<br />
+			<select name="blog-name-characters" id="blogs-blog-name-characters" style="width:95%;">
+			<?php
+			if ( empty($options['blog-name-characters']) ) {
+				$options['blog-name-characters'] = 30;
+			}
+			$counter = 0;
+			for ( $counter = 1; $counter <= 500; $counter += 1) {
+			?>
+				<option value="<?php echo $counter; ?>" <?php if ($options['blog-name-characters'] == $counter){ echo 'selected="selected"'; } ?> ><?php echo $counter; ?></option>
+                        <?php
+			}
+			?>
+			</select>
+                </label>
+		<label for="blogs-order" style="line-height:35px;display:block;"><?php _e('Order', 'widgets', 'widget_blogs'); ?>:
+			<select name="order" id="blogs-order" style="width:95%;">
+				<option value="most_recent" <?php if ($options['order'] == 'most_recent'){ echo 'selected="selected"'; } ?> ><?php _e('Most Recent', 'widget_blogs'); ?></option>
+				<option value="random" <?php if ($options['order'] == 'random'){ echo 'selected="selected"'; } ?> ><?php _e('Random', 'widget_blogs'); ?></option>
+			</select>
+                </label>
+		<label for="blogs-number" style="line-height:35px;display:block;"><?php _e('Number', 'widgets', 'widget_blogs'); ?>:<br />
+			<select name="blogs-number" id="number" style="width:95%;">
+			<?php
+			if ( empty($options['number']) ) {
+				$options['number'] = 10;
+			}
+			$counter = 0;
+			for ( $counter = 1; $counter <= 25; $counter += 1) {
+			?>
+				<option value="<?php echo $counter; ?>" <?php if ($options['number'] == $counter){ echo 'selected="selected"'; } ?> ><?php echo $counter; ?></option>
+			<?php
+			}
+			?>
+			</select>
+                </label>
+		<?php if (function_exists('get_blog_avatar')) { ?>
+		<label for="blogs-avatar-size" style="line-height:35px;display:block;"><?php _e('Avatar Size', 'widgets', 'widget_blogs'); ?>:<br />
+			<select name="avatar-size" id="blogs-avatar-size" style="width:95%;">
+			<option value="16" <?php if ($options['avatar-size'] == '16'){ echo 'selected="selected"'; } ?> ><?php _e('16px', 'widget_blogs'); ?></option>
+			<option value="32" <?php if ($options['avatar-size'] == '32'){ echo 'selected="selected"'; } ?> ><?php _e('32px', 'widget_blogs'); ?></option>
+			<option value="48" <?php if ($options['avatar-size'] == '48'){ echo 'selected="selected"'; } ?> ><?php _e('48px', 'widget_blogs'); ?></option>
+			<option value="96" <?php if ($options['avatar-size'] == '96'){ echo 'selected="selected"'; } ?> ><?php _e('96px', 'widget_blogs'); ?></option>
+			<option value="128" <?php if ($options['avatar-size'] == '128'){ echo 'selected="selected"'; } ?> ><?php _e('128px', 'widget_blogs'); ?></option>
+			</select>
+		</label>
+		<?php } else { ?>
+			<input type="hidden" name="avatar-size" id="blogs-avatar-size" value="16" />
+		<?php } ?>
+		<input type="hidden" name="submit" id="submit" value="1" />
+	</div>
+	<?php
+    }
 }
-
-add_action('widgets_init', 'widget_blogs_widget_init');
 
 if ( !function_exists( 'wdp_un_check' ) ) {
 	add_action( 'admin_notices', 'wdp_un_check', 5 );
